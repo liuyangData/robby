67cad6a690e2f71f7d5dbf8d723121d3df8b1130
LiuYang <46698230+liuyangData@users.noreply.github.com>
2020-11-12 04:00:57 +0800

    Update alanTuringTest.py

--- alanTuringTest.py
+++ alanTuringTest.py
@@ -1,89 +1,83 @@
 from chatbot import Chatbot
 import json
+import random
 
+agents = ["IR", "ML", "RB", "HM"]
 
-print("Choose an agent: IR, ML, RB")
-agent = input()
+for agent in random.shuffle(agents) :
 
+    ### Information Retrieval System
 
-### Information Retrieval System
+    if agent == 'IR':
 
+        print("Say something")
 
-if agent == 'IR':
+        from chatbot import IR_Bot
 
-    print("Meet the IR Bot. Say something")
+        ir = IR_Bot('ir')
+        ir.loadDictJson('irDictionary.json')
 
-    from chatbot import IR_Bot
-
-    ir = IR_Bot('ir')
-    ir.loadDictJson('irDictionary.json')
-
-    userResponse = input()
-    while 'quit' not in userResponse:
-        print(ir.reply(userResponse))
         userResponse = input()
+        while 'quit' not in userResponse:
+            print(ir.reply(userResponse))
+            userResponse = input()
 
 
-# Machine Learning Bot
+    # Machine Learning Bot
 
-if agent == 'ML':
+    if agent == 'ML':
 
-    print("Meet the ML Bot. Say something:")
+        print("Say something:")
 
-    from chatbot import ML_Bot
+        from chatbot import ML_Bot
 
-    ml = ML_Bot("ML")
-    ml.trainJSON('convAI.json')
+        ml = ML_Bot("ML")
+        ml.trainJSON('convAI.json')
 
-    userResponse = input()
-    while 'quit' not in userResponse:
-        print(ml.reply(userResponse))
         userResponse = input()
+        while 'quit' not in userResponse:
+            print(ml.reply(userResponse))
+            userResponse = input()
 
 
-# Hybrid Chatbot: Robby
+    # Hybrid Chatbot: Robby
 
-if agent == 'RB':
-    
-    print("Meet Robby. Say something:")
-    res = input()
+    if agent == 'RB':
 
-    while res != 'bye':
-        print(model.get_response(res))
+
+        from chatterbot import ChatBot
+        from chatbot import Robby
+        robby = Robby("Rob")
+
+        robby.mind.loadMindPalace('mindPalace.json')
+        robby.initiateTopic()
+
+        robby.markov.loadCorpus('convAI.json')
+
+        from markovChain import MarkovChain
+        m = MarkovChain()
+        m.loadCorpus('convAI.json')
+
+        robby.loadNeuralNetwork('intents.json')
+
+        print("Say something:")
         res = input()
 
+        while res != 'bye':
+            print(robby.neuralNetResponse(res))
+            res = input()
+            
+        
+    ### Human Chat
+    if agent == 'HM': 
+        print("Say something")
+        userResponse = input()
+        while 'quit' not in userResponse:
+            print(getInput())
+            userResponse = input()
+
+           
 
 
 
 
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
-from chatterbot.trainers import UbuntuCorpusTrainer
-
-model = ChatBot(name='robby',  
-        logic_adapters=['chatterbot.logic.BestMatch'],
-        storage_adapter='chatterbot.storage.SQLStorageAdapter',
-        database_uri='sqlite:///database.sqliteRobby')
-
-ubuntu_trainer = UbuntuCorpusTrainer(model)
-ubuntu_trainer.train()
-
-
-#### 
-
-from chatbot import Robby
-robby = Robby("Rob")
-
-robby.mind.loadMindPalace('mindPalace.json')
-robby.initiateTopic()
-
-robby.markov.loadCorpus('convAI.json')
-robby.mutter()
-
-from markovChain import MarkovChain
-m = MarkovChain()
-m.loadCorpus('convAI.json')
-
-robby.loadNeuralNetwork('intents.json')
-robby.neuralNetResponse('hello')
